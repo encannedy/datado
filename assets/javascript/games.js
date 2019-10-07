bd23d10605b4510f9c33fdc9832dbe4733e2ce80
Erica <encannedy@gmail.com>
2019-10-06 21:27:42 -0400

    js updates including array and variables named

---
+++ assets/javascript/games.js
@@ -0,0 +1,12 @@
+
+//create an array for the options for the computer choice.
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "P", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+ // Create variables that hold references to the places in the HTML where we want to display things.
+var wins = 0;
+var loses = 0;
+var lettersGuessed = [];
+var guessesLeft = 5;
+
+console.log (computerChoice);
